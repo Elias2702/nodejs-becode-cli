d4a53342e04f0e33002e31eb4336cf257dbef707
Elias <elias.djenidi@gmail.com>
2019-01-23 09:53:24 +0100

    some cleaning

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nodejs-becode-cli
+
+Checks any email address against the haveibeenpwned.com database and reports back.
+If your address has been compromised, make sure to read the full report on haveibeenpwned.com and take appropriate measures.
+
+Safe travels!;)
+
+## Installation
+Have node.js and npm installed, and run:
+```console
+npm i @kaidan/breached
+```
--- index.js
+++ index.js
@@ -1,48 +1,51 @@
 #!/usr/bin/env node
-let axios = require('axios');
-const chalk = require('chalk');
+const axios = require('axios')
+const validator = require("email-validator")
+const chalk = require('chalk')
 
-// Retrieving email address from terminal user
-if (process.argv[2] == null) {console.log('Please enter a valid email address after testouille to check for breaches.')}
-else {
-let input_email = process.argv[2];
+// Retrieving email address from user terminal
+let input_email;
+if (process.argv[2] == null) {
+    console.log('Please enter a valid email address after breached to check for breaches.')
+} else {
+    input_email = process.argv[2]
+}
 
 // Validating email format
-let validator = require("email-validator");
-let validated = validator.validate(input_email);
+const validated = validator.validate(input_email)
 
 //  Format response to user
-let validated_response = '';
+let validated_response;
 if (validated) {
-    validated_response = 'This is a valid email address format. Well done ! ;)';
+    validated_response = 'This is a valid email address format. Well done ! ;)'
 } else {
-    validated_response = 'This is not a valid email address format. Try harder.';
+    validated_response = 'This is not a valid email address format. Try harder.'
 }
-console.log(validated_response);
+console.log(validated_response)
 
 // Axios request to haveibeenpwned.com
 
     // Config
-let url = `https://haveibeenpwned.com/api/v2/breachedaccount/${input_email}`;
-let headers = {'api-version': '2', 'user-agent': 'Oufti'};
+const url = `https://haveibeenpwned.com/api/v2/breachedaccount/${input_email}`
+const headers = {'api-version': '2', 'user-agent': 'Oufti'}
 
     // Request and response
 if (validated) {
-    console.log(`Requesting to: ${url}`);
+    console.log(`Requesting to: ${url}`)
     axios.get(`${url}`, {headers: headers})
     .then(function (response) {
-              console.log(`Connecting to server: okay (status code ${response.status})`);
-              const breaches=response.data.map(({Name}) => Name);
-              console.log(chalk.bgRed(`Your email address has been breached from databases of: ${breaches.join("\n")}`))
+              console.log(`Connecting to server: okay (status code ${response.status})`)
+              const breaches=response.data.map(({Name}) => Name)
+              console.log(chalk.bgRed(`Your email address has been breached from the following databases: ${breaches.join("\n")}`))
       })
     .catch(function (error) {
         if (error.response) {
-            console.log(chalk.bgGreen('Great news! It looks like your email address has not been breached.'));
+            console.log(`Connecting to server: okay`)
+            console.log(chalk.bgGreen('Great news! It looks like your email address has not been breached.'))
         } else if (error = 429) {
-            console.log('You are either making too many requests or your are disconnected. Wait for a bit, check your connection and try again.');
+            console.log('You are either making too many requests or your are disconnected. Wait for a bit, check your connection and try again.')
         } else {
-            console.log('An unknown error has occured, and this is highly embarrassing.');
+            console.log('An unknown error has occured, and this is highly embarrassing.')
         }
     })
-}
 }
